177ef594363d63d996151c22cc17458b6f5081a0
hanitaher0106 <hani.taher2000@gmail.com>
2024-07-01 10:13:24 +0100

    Second commit

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
-})
+const fastify = require('fastify')({ logger: true });
+
+
+const sequelize = require('./config/database');
+
+const PORT = 3000;
+
 
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
-    return { hello: 'world' }
+    return { msg: 'hello from my project fastify !!' }
   })
 
 
-// Run the server!
-try {
-    await fastify.listen({ port: 3000 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+  const start = async () => {
+    try {
+      await fastify.listen({ port: PORT });
+      console.log(`Server is running on port ${PORT}`);
+      await sequelize.authenticate();
+      console.log('Connection  successfully.');
+      // await sequelize.sync(); 
+      await createInitialData();
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      fastify.log.error(error);
+      process.exit(1);
+    }
+  };
+  
+  start();
